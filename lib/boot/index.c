16507b48244e5f5c0b5c53fb70a58b01a26970ba
Yarot Kell <mechild02@gmail.com>
2022-07-12 16:49:36 -0400

    [LIBC] begin work on stdatomics.

--- lib/boot/index.c
+++ lib/boot/index.c
@@ -1,9 +1,22 @@
-extern void kernel_start(void) {
+#include<stdint.h>
+
+// Our kernel entry point takes a pointer to a KernelArgs struct, which passes the size parameters
+// of our kernel and the heap.
+void kernel_start(KernelArgs* args_ptr) {
    extern {
-      void kernel_main(void);
+      void kernel_main(uint64_t cpus, const uint8_t** env);
    }
 
    kernel_main();
 
    return;
 }
+
+typedef struct __attribute__((packed)) {
+   int64_t kernel_base;
+   int64_t kernel_size;
+   int64_t heap_base;
+   int64_t heap_size;
+   int64_t env_base;
+   int64_t env_size;
+} KernelArgs;
