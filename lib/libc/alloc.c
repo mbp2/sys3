244d2a50c13b9aae6a6056f3ae5f5ff045beff88
Yarot Kell <mechild02@gmail.com>
2022-10-25 12:41:56 -0400

    [SYS3] adding heap setup.

--- lib/libc/alloc.c
+++ lib/libc/alloc.c
@@ -1,8 +1,72 @@
 #include <libc/alloc.h>
+#include <libc/mem.h>
 
 #define MAX_ALLOC_ALLOWED 20
 
-MallocInfo metadata_info[MAX_ALLOC_ALLOWED] = {0};
+extern size_t _heap_size;
 
-void* malloc(size_t size) {
+static uint32_t AllocatedNumber = 0;
+static uint32_t HeapBaseAddr = 0x0;
+static unsigned char memory_stat[_heap_size];
+AllocInfo metadata_info[MAX_ALLOC_ALLOWED] = {0};
+
+void* allocate(size_t size) {
+   int j = 0;
+   int index = 0;
+   int initial_gap = 0;
+   int gap = 0;
+   int heap_index = 0;
+
+   void* addr = NULL;
+
+   bool flag = false;
+   bool initial_flag = false;
+
+   AllocInfo temp_info = {0};
+
+   if (AllocatedNumber >= MAX_ALLOC_ALLOWED) {
+      return NULL;
+   }
+
+   for (index = 0; index < AllocatedNumber; index++) {
+      if (metadata_info[index+1].address != 0){
+         initial_gap = metadata_info[0].address - HeapBaseAddr;
+         if (initial_gap >= size) {
+            initial_flag = true;
+            break;
+         } else {
+            gap = metadata_info[index + 1].address - (metadata_info[index].address + metadata_info[index].size);
+            if (gap >= size) {
+               flag = true;
+               break;
+            }
+         }
+      }
+   }
+
+   if (flag == true) { /* Get Index for allocating memory for case two. */
+      heap_index = ((metadata_info[index].address + metadata_info[index].size) - HeapBaseAddr);
+
+      for (j = MAX_ALLOC_ALLOWED - 1; j > index + 1; j--) {
+         MemCpy(&metadata_info[j], &metadata_info[j - 1], sizeof(AllocInfo));
+      }
+   } else if (initial_flag == true) { /* Get index for allocating memory for case three. */
+      heap_index = 0;
+
+      for (j = MAX_ALLOC_ALLOWED - 1; j > index + 1; j--) {
+         MemCpy(&metadata_info[j], &metadata_info[j - 1], sizeof(AllocInfo));
+      }
+   } else { /* Get Index for allocating memory for case one. */
+      if (AllocatedNumber != 0) {
+         heap_index = ((metadata_info[index - 1].address - metadata_info[index - 1].size) - HeapBaseAddr);
+      } else {
+         heap_index = 0;
+      }
+   }
+
+   address = &memory_stat[heap_index];
+   metadata_info[index].address = HeapBaseAddr + heap_index;
+   metadata_info[index].size = size;
+
+   return address;
 }
--- lib/sys3/shell/vga.c
+++ lib/sys3/shell/vga.c
@@ -1,1 +1,9 @@
 #include "vga.h"
+
+static inline uint8_t VgaColour(enum VGA fg, enum VGA bg) {
+   return fg | bg << 4;
+}
+
+static inline uint16_t VgaEntry(unsigned char uc, uint8_t colour) {
+   return (uint16_t) uc | (uint16_t) colour << 8;
+}
