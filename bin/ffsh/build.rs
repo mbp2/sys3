9ec76a65127d532a04b5943e26cb9d676def636a
Az <azyklus@disroot.org>
2023-12-05 22:43:47 -0500

    [SHELL] adding zig library for shell project.

--- bin/ffsh/build.rs
+++
@@ -1,9 +0,0 @@
-/// Builds the SYSTEM-3 `libc` implementation and places the built
-/// artifact into the build directory.
-fn main() -> io::Result<()> {
-   return Ok(());
-}
-
-// MODULES ////////////////////////////////////////////////////////////////////////////////////////
-
-use std::io;
