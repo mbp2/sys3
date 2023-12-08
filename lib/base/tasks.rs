26a313782ba508bad90c541758d88bf59c036c23
Az <azyklus@disroot.org>
2023-12-07 19:00:33 -0500

    [BASE] fixed task executor.

--- lib/base/tasks.rs
+++ lib/base/tasks.rs
@@ -1,28 +1,84 @@
-pub struct Task {
-   pub id: TaskId,
-   pub future: Pin<Unique<dyn Future<Output = ()>>>,
+/// Polls all pending tasks on global executor and remove completed tasks.
+pub fn run_tasks() {
+   executor::DEFAULT_EXECUTOR.lock().run();
 }
 
-impl Task {
-   pub fn new(future: impl Future<Output = ()> + 'static) -> Task {
-      return Task{
-         id: TaskId::new(),
-         future: Unique::pin(future),
-      };
+/// Adds task for a future to the global executor queue.
+pub fn add_future<T>(future: impl Future<Output = T> + 'static + Send)
+where
+   T: Send + 'static, {
+   executor::DEFAULT_EXECUTOR.lock().add_async(Box::pin(future));
+}
+
+/// Drops completed tasks and checks if any uncompleted tasks remain.
+pub fn completed() -> bool {
+   let mut executor = executor::DEFAULT_EXECUTOR.lock();
+   executor.collect();
+   return executor.tasks.is_empty();
+}
+
+/// Adds task for a future to the executor queue and immediately polls it.
+pub fn poll_now<T>(future: impl Future<Output = T> + 'static + Send)
+where
+   T: Send + 'static, {
+   executor::DEFAULT_EXECUTOR.lock().poll_now(Box::pin(future));
+}
+
+pub type TaskList = VecDeque<Box<dyn Pendable + core::marker::Send + core::marker::Sync>>;
+
+/// Container for [`Future`] and [`Future`]'s state, like [`Task::completed`].
+///
+/// Task is our unit of execution and holds a future to wait on.
+pub struct Task<T> {
+   pub future: Spinlock<Pin<Box<dyn Future<Output = T> + Send + 'static>>>,
+   pub completed: AtomicBool,
+}
+
+pub trait Pendable {
+   /// Updates future progress via calling [`Future::poll()`].
+   ///
+   /// Shall contain future's status internally and corresponding to [`Future::poll`] state.
+   fn update(&self);
+
+   /// Returns `true` if the state is corresponding to [`core::task::Poll::Ready`] otherwise - false.
+   ///
+   /// Needed to determine, which task we shall drop.
+   fn is_done(&self) -> bool;
+}
+
+impl<T> Wake for Task<T> {
+   fn wake(self: Arc<Self>) {
+      self.update();
    }
 
-   pub fn poll(&mut self, context: &mut Context) -> Poll<()> {
-      return self.future.as_mut().poll(context);
+   fn wake_by_ref(self: &Arc<Self>) {
+      self.update();
    }
 }
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq, PartialOrd, Ord)]
-pub struct TaskId(pub u64);
+impl<T> ArcWake for Task<T> {
+   fn wake(self: Arc<Self>) {
+      self.update();
+   }
 
-impl TaskId {
-   pub fn new() -> Self {
-      static NEXT_ID: AtomicU64 = AtomicU64::new(0);
-      TaskId(NEXT_ID.fetch_add(1, Ordering::Relaxed))
+   fn wake_by_ref(arc_self: &Arc<Self>) {
+      arc_self.update();
+   }
+}
+
+impl<T> Pendable for Arc<Task<T>> {
+   fn update(&self) {
+      let mut future = self.future.lock();
+      let waker = waker_ref(self);
+      let context = &mut Context::from_waker(&waker);
+      self.completed.store(
+         !matches!(future.as_mut().poll(context), Poll::Pending),
+         Ordering::Relaxed,
+      );
+   }
+
+   fn is_done(&self) -> bool {
+      return self.completed.load(Ordering::Relaxed);
    }
 }
 
@@ -34,13 +90,22 @@ pub mod keyboard;
 // IMPORTS //
 
 use {
-   crate::pointer::Unique,
    core::{
       future::Future,
       pin::Pin,
-      sync::atomic::{AtomicU64, Ordering},
+      sync::atomic::{AtomicBool, Ordering},
       task::{Context, Poll},
+   },
+   futures_util::task::{ArcWake, waker_ref},
+   spinning_top::Spinlock,
+   std_alloc::{
+      boxed::Box,
+      collections::VecDeque,
+      sync::Arc,
+      task::Wake
    },
 };
 
 // EXPORTS //
+
+pub use self::executor::Executor;
--- lib/base/tasks/executor.rs
+++ lib/base/tasks/executor.rs
@@ -1,100 +1,94 @@
+/// The default global task executor.
+pub static DEFAULT_EXECUTOR: Spinlock<Executor> = Spinlock::new(Executor::new());
+
+/// Our executor type.
 pub struct Executor {
-   pub tasks: BTreeMap<TaskId, Task>,
-   pub task_queue: Arc<ArrayQueue<TaskId>>,
-   pub waker_cache: BTreeMap<TaskId, Waker>,
+   /// A collection of tasks to execute.
+   pub tasks: TaskList,
 }
 
 impl Executor {
-   pub fn new() -> Self {
+   pub const fn new() -> Self {
       return Executor{
-         tasks: BTreeMap::new(),
-         task_queue: Arc::new(ArrayQueue::new(100)),
-         waker_cache: BTreeMap::new(),
+         tasks: VecDeque::new(),
       };
    }
 
-   pub fn spawn(&mut self, task: Task) {
-      let task_id = task.id;
-      if self.tasks.insert(task.id, task).is_some() {
-         log::error!("A task with the same identifier already exists");
+   pub fn add_task<T>(&mut self, task: Arc<Task<T>>)
+   where
+      T: Send + 'static, {
+      self.tasks.push_back(Box::new(task));
+   }
+
+   /// Add a [`Task<T>`](crate::task::Task) to the executor queue and immediately poll it.
+   pub fn poll_now<T>(&mut self, future: Pin<Box<dyn Future<Output = T> + 'static + Send>>)
+   where
+      T: Send + 'static {
+      let task = Arc::new(Task{
+         future: Spinlock::new(future),
+         completed: AtomicBool::new(false),
+      });
+
+      task.update();
+      self.add_task(task);
+   }
+
+   /// Add task for a future to the executor queue.
+   pub fn add_async<T>(&mut self, future: Pin<Box<dyn Future<Output = T> + 'static + Send>>)
+   where
+      T: Send + 'static {
+      let task = Arc::new(Task{
+         future: Spinlock::new(future),
+         completed: AtomicBool::new(false),
+      });
+
+      self.add_task(task);
+   }
+
+   /// Polls all pending tasks on global executor and remove completed tasks.
+   ///
+   /// You may notice, that when all tasks will done, we keep them, although this is objectively
+   /// useless. I think, finding our each completed task from [`Wake::wake_by_ref()`] at [`Executor::tasks`]
+   /// and drop it is too expensive, so we just mark them via [`Task<T>::completed`] field as done.
+   /// When all tasks will done and we add new task and run them, old completed tasks will be removed
+   /// from [`Executor::tasks`] or [`Executor::collect()`].
+   pub fn run(&mut self) {
+      for _ in 0..self.tasks.len() {
+         let task = self.tasks.pop_front().unwrap();
+         if !task.is_done() {
+            task.update();
+            self.tasks.push_back(task);
+         }
       }
-
-      self.task_queue.push(task_id).expect("queue full");
    }
 
-   pub fn run(&mut self) -> ! {
-      loop{
-         self.run_ready_tasks();
-         self.sleep_if_idle();
+   /// Removes completed task from [`Executor::tasks`].
+   ///
+   /// As you may also notice, same as [`Executor::run()`], but don't poll tasks.
+   /// Only drop completed tasks.
+   pub fn collect(&mut self) {
+      for _ in 0..self.tasks.len() {
+         let task = self.tasks.pop_front().unwrap();
+         if !task.is_done() {
+            self.tasks.push_back(task);
+         }
       }
-   }
-
-   pub fn run_ready_tasks(&mut self) {
-      // Destructure self to avoid borrow checker errors.
-      let Self {
-         tasks,
-         task_queue,
-         waker_cache,
-      } = self;
-
-      while let Some(task_id) = task_queue.pop() {
-         let task = match tasks.get_mut(&task_id) {
-            Some(task) => task,
-            None => continue,
-         };
-
-         let waker = waker_cache.entry(task_id)
-            .or_insert_with(|| TaskWaker::new(task_id, task_queue.clone()));
-
-         let mut context = Context::from_waker(waker);
-      }
-   }
-
-   pub fn sleep_if_idle(&self) {
-      use x86_64::instructions::interrupts::{self, enable_and_hlt};
-
-      interrupts::disable();
-      if self.task_queue.is_empty() {
-         enable_and_hlt();
-      } else {
-         interrupts::enable();
-      }
-   }
-}
-
-pub struct TaskWaker {
-   pub task_id: TaskId,
-   pub task_queue: Arc<ArrayQueue<TaskId>>,
-}
-
-impl TaskWaker {
-   pub fn new(task_id: TaskId, task_queue: Arc<ArrayQueue<TaskId>>) -> Waker {
-      Waker::from(Arc::new(TaskWaker {
-         task_id,
-         task_queue,
-      }))
-   }
-
-   pub fn wake_task(&self) {
-      self.task_queue.push(self.task_id).expect("task queue full");
-   }
-}
-
-impl Wake for TaskWaker {
-   fn wake(self: Arc<Self>) {
-      self.wake_task();
-   }
-
-   fn wake_by_ref(self: &Arc<Self>) {
-      self.wake_task();
    }
 }
 
 // IMPORTS //
 
 use {
-   super::{Task, TaskId},
-   std_alloc::{collections::BTreeMap, sync::Arc, task::Wake},
-   core::task::{Context, Poll, Waker},
-   crossbeam_queue::ArrayQueue,
+   super::{Pendable, Task, TaskList},
+   core::{
+      future::Future,
+      pin::Pin,
+      sync::atomic::AtomicBool,
+   },
+   spinning_top::Spinlock,
+   std_alloc::{
+      boxed::Box,
+      collections::VecDeque,
+      sync::Arc,
+   },
 };
