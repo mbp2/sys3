41326f2e33b54804d759ac89e6f0c2d3623a1687
Az <azyklus@disroot.org>
2023-12-07 19:24:59 -0500

    [V3] add init for COM2 port in x86_64 arch module.

--- main/arch/x86_64.rs
+++ main/arch/x86_64.rs
@@ -2,12 +2,17 @@
 pub fn initialise_platform() {
    log::info!("Detected x86_64 CPU!");
    log::info!("Moving to initialise x86_64 platform modules.");
+
+   COM2.lock().initialise();
 }
 
 // IMPORTS //
 
 use {
-   base::log,
+   base::{
+      log,
+      uart::COM2,
+   },
 };
 
 // MODULES //
